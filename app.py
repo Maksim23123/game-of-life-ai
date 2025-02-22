cfe72ba23dc2f1ba705fa351002769b755d799c1
Maksim23123 <102969640+Maksim23123@users.noreply.github.com>
2025-02-22 19:32:22 +0100

    refactor: organize MainWindow class structure

--- app.py
+++ app.py
@@ -8,17 +8,27 @@ class MainWindow(QtWidgets.QMainWindow):
         super().__init__()
         
         window = QtWidgets.QWidget()
-        main_layout =QtWidgets.QHBoxLayout()
+        self.main_layout = QtWidgets.QHBoxLayout()
         
-        self.label = QtWidgets.QLabel()
+        self._init_canvas_holder()
+        self._init_gui_elements()
+        self._init_timer()
+
+        window.setLayout(self.main_layout)
+        self.setCentralWidget(window)
+    
+    
+    def _init_canvas_holder(self):
+        self.canvas_holder = QtWidgets.QLabel()
         canvas = QtGui.QPixmap(400, 300)
         canvas.fill(Qt.GlobalColor.white)
-        self.label.setPixmap(canvas)
-        self.setCentralWidget(self.label)
+        self.canvas_holder.setPixmap(canvas)
+        self.setCentralWidget(self.canvas_holder)
         self.draw_something()
-        
+    
+    
+    def _init_gui_elements(self):
         control_panel_layout = QtWidgets.QVBoxLayout()
-        
         
         self.start_button = QtWidgets.QPushButton("Start")
         self.stop_button = QtWidgets.QPushButton("Stop")
@@ -31,24 +41,22 @@ class MainWindow(QtWidgets.QMainWindow):
         control_panel_layout.addWidget(self.stop_button)
         control_panel_layout.addWidget(self.test_label)
         
-        main_layout.addLayout(control_panel_layout)
-        main_layout.addWidget(self.label)
+        self.main_layout.addLayout(control_panel_layout)
+        self.main_layout.addWidget(self.canvas_holder)
         
+
+    def _init_timer(self):
         self.timer = QtCore.QTimer()
         self.timer.timeout.connect(self.update)
-        self.timer.start(100)
-
-        window.setLayout(main_layout)
-        self.setCentralWidget(window)
+        self.timer.start(1)
     
     
     def update(self):
         self.x += 1
         self.test_label.setText(str(self.x))
-        
 
     def draw_something(self):
-        canvas = self.label.pixmap()
+        canvas = self.canvas_holder.pixmap()
         painter = QtGui.QPainter(canvas)
         pen = QtGui.QPen()
         pen.setWidth(40)
@@ -56,7 +64,7 @@ class MainWindow(QtWidgets.QMainWindow):
         painter.setPen(pen)
         painter.drawPoint(200, 150)
         painter.end()
-        self.label.setPixmap(canvas)
+        self.canvas_holder.setPixmap(canvas)
 
 
 app = QtWidgets.QApplication(sys.argv)
